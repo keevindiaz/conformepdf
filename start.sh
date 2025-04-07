9319a6b5abd330a662082c944ca40516814b9b50
keevindiaz <83251927+keevindiaz@users.noreply.github.com>
2025-04-07 19:44:38 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-chmod +x ./bin/wkhtmltopdf   # << Esto agrega el permiso de ejecución EN RENDER
-echo "Verificando permisos..."
-ls -l ./bin/wkhtmltopdf
-file ./bin/wkhtmltopdf
-
-# Iniciar el servidor con gunicorn
-gunicorn app:app --bind 0.0.0.0:10000
+chmod +x ./bin/wkhtmltopdf
+gunicorn app:app
